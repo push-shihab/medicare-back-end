89d37b8ae8c5286e3a181974d01d80ac922b34fc
push-shihab <shihabfamily539@gmail.com>
2026-06-27 00:47:38 +0600

    deploying on vercel

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("medicare");
     const userCollection = db.collection("user");
@@ -594,7 +594,7 @@ async function run() {
       res.json(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
